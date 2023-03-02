312a0b6afea17bb1e44e6eff2111f121e974004f
ImagineGamingPlay <50368787+ImagineGamingPlay@users.noreply.github.com>
2023-03-02 20:18:29 +0530

    Update authors.ts

--- src/util/authors.ts
+++ src/util/authors.ts
@@ -1,1 +1,1 @@
-export const AUTHORS = ['Duro', 'JadeBetty', 'shaken-bird', 'IHZAQ', 'LonelyBall', 'Aljo'] as const;
+export const AUTHORS = ['Duro', 'JadeBetty', 'shaken-bird', 'IHZAQ', 'LonelyBall', 'Aljo', "IGP"] as const;
